ef9610708f6516d7cf8c3874a74f280ef192b4f0
Robson <robson@webstationdesign.com.br>
2019-10-25 21:38:30 -0300

    - Configurando Nodemailer - Configurando templates de e-mail

---
+++ desenvolvimento/backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "e664d3d184b903",
+    pass: "0d950fc5f4b9be"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  }
+};
