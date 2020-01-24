b599823b611b5459bb8abd4d13c1d774a9b53119
Robson <robson@webstationdesign.com.br>
2020-01-23 22:05:04 -0300

    Rotas privadas

--- desenvolvimento/frontend/src/App.js
+++ desenvolvimento/frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
 
 import Routes from './routes';
+import history from './services/history';
+
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
-      <Routes />
+      <Router history={history}>
+        <Routes />
+      </Router>
       <GlobalStyle />
     </>
   );
